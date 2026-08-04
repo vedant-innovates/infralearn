89844392eb605cf2f67397ed3faf2ba543b8a12c
Vedant <vedant.innovates@gmail.com>
2026-08-04 23:00:02 +0400

    my demo

---
+++ demo/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Specify the EC2 details
+resource "aws_instance" "example" {
+  ami           = "ami-01a00762f46d584a1"
+  instance_type = "t3.micro"
+}
+
+# Create S3 bucket
+resource "aws_s3_bucket" "example" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts
+  bucket = "vinnov-demo-s3-basic"
+}
